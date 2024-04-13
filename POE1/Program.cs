fccaa4476a0cea9a550892bc9dfbb4135c430b9d
Yadav Priaram <131756403+st10356506@users.noreply.github.com>
2024-04-13 20:50:18 +0200

    Add files via upload

--- POE1/Program.cs
+++ POE1/Program.cs
@@ -104,4 +104,22 @@ namespace RecipeApplication
         }
     }
 
-    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+
+            Console.WriteLine("Welcome to Recipe Book!");
+
+            do
+            {
+                Console.WriteLine("\nSelect a function:");
+                Console.WriteLine("(1) Create a new recipe");
+                Console.WriteLine("(2) Display recipe");
+                Console.WriteLine("(3) Scale recipe");
+                Console.WriteLine("(4) Reset quantities");
+                Console.WriteLine("(5) Clear recipe");
+                Console.WriteLine("(6) Quit");
+
+               
