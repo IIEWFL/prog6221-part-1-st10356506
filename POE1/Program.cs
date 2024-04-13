ca86fec018421df6c8e89f1abad2ddf29afe50d0
Yadav Priaram <131756403+st10356506@users.noreply.github.com>
2024-04-13 20:39:35 +0200

    Add files via upload

--- POE1/Program.cs
+++ POE1/Program.cs
@@ -21,4 +21,34 @@ namespace RecipeApplication
         public string Steps { get; set; }
     }
 
-   
+    class Recipe
+    {
+        public Ingredient[] ingredients;
+        public RecipeStep[] steps;
+
+        public Recipe()
+        {
+            ingredients = new Ingredient[0];
+            steps = new RecipeStep[0];
+        }
+
+        public void CreateRecipe()
+        {
+            Console.WriteLine("Enter details for the recipe:");
+
+            Console.Write("Enter number of ingredients: ");
+            int ingredientCount = int.Parse(Console.ReadLine());
+            ingredients = new Ingredient[ingredientCount];
+            for (int i = 0; i < ingredientCount; i++)
+            {
+                Console.WriteLine($"Enter ingredient details {i + 1}:");
+                Console.Write("Ingredient name: ");
+                string name = Console.ReadLine();
+                Console.Write("Ingredient quantity: ");
+                double quantity = double.Parse(Console.ReadLine());
+                Console.Write("Unit of measurement: ");
+                string unit = Console.ReadLine();
+                ingredients[i] = new Ingredient { Name = name, Quantity = quantity, Unit = unit };
+            }
+
+           
