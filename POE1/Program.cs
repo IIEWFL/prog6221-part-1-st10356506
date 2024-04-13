4e238f6d49650d5dd81cf9b763971a24a3814e9a
Yadav Priaram <131756403+st10356506@users.noreply.github.com>
2024-04-13 20:36:46 +0200

    Add files via upload

---
+++ POE1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*method for creating recipe
+ * method for creating, viewing, scaling, clearing, resetting recipe
+ * call these methods in main method
+ * use conditional or switch cases
+ * use a menu-use part 3 2023 as a reference-welcome message
+ 
+*/
+using System;
+
+namespace RecipeApplication
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+
+    class RecipeStep
+    {
+        public string Steps { get; set; }
+    }
+
+   
